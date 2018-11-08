60d926c662f652283b3636fa4363067621ff649c
John <xxssww0258@sina.com>
2018-11-08 19:45:06 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # express-architecture-demo
-express-architecture-demo
+# express-architecture-demo
---
+++ router/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+
+require('./user')(router)
+
+module.exports = router
